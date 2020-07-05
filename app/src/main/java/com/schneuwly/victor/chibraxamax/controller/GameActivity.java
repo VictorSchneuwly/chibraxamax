fdf12f29e1af23a696c8bf7ddfef27c35553028b
vschneuwly <victor.schneuwly@etude-ssk.ch>
2020-07-05 21:44:18 +0200

    Modification de valeur

--- app/src/main/java/com/schneuwly/victor/chibraxamax/controller/GameActivity.java
+++ app/src/main/java/com/schneuwly/victor/chibraxamax/controller/GameActivity.java
@@ -1,9 +1,11 @@
 package com.schneuwly.victor.chibraxamax.controller;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.text.InputType;
 import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.schneuwly.victor.chibraxamax.R;
 import com.schneuwly.victor.chibraxamax.model.Duo;
@@ -12,47 +14,67 @@ import com.schneuwly.victor.chibraxamax.model.Player;
 
 public class GameActivity extends AppCompatActivity {
     //private TextView team1_20, team1_50, team1_100, team2_20, team2_50, team2_100;
-    private EditText scoreView, team1_name, team2_name;
+    private TextView scoreView, team1_name, team2_name;
+    private Player[] players;
+    private Duo[] duos;
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        Player[] players = {
+        players = new Player[]{
                 new Player("Player 1"),
                 new Player("Player 2"),
                 new Player("Player 3"),
                 new Player("Player 4")
         };
 
-        Duo[] duos = {
+        duos = new Duo[]{
                 new Duo(players[0], players[1], getResources().getString(R.string.default_name_team1)),
                 new Duo(players[2], players[3], getResources().getString(R.string.default_name_team2))
         };
 
-        Game game = new Game(duos[0], duos[1], Integer.parseInt(getResources().getString(R.string.default_score)));
+        game = new Game(duos[0], duos[1], Integer.parseInt(getResources().getString(R.string.default_score)));
 
         scoreView = findViewById(R.id.score);
-        scoreView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        scoreView.setOnClickListener(l -> {
+            EditText input = new EditText(this);
+            input.setInputType(InputType.TYPE_CLASS_NUMBER);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
+            showPopUp("Score", "Changer le score final:", input,
+                    (d, w) -> {
+                        int newScore = Integer.parseInt(input.getText().toString());
+                        game.setEndScore(newScore);
+                        scoreView.setText(input.getText().toString());
+                    }
+            );
         });
 
         team1_name = findViewById(R.id.team1_name);
+        team1_name.setOnClickListener(l -> {
+            EditText input = new EditText(this);
+
+            showPopUp(duos[0].getName(), "Changer le nom de " + duos[0].getName() + ":", input,
+                    (d, w) -> {
+                        duos[0].setName(input.getText().toString());
+                        team1_name.setText(input.getText().toString());
+                    }
+            );
+        });
+
         team2_name = findViewById(R.id.team2_name);
+        team2_name.setOnClickListener(l -> {
+            EditText input = new EditText(this);
+
+            showPopUp(duos[1].getName(), "Changer le nom de " + duos[1].getName() + ":", input,
+                    (d, w) -> {
+                        duos[1].setName(input.getText().toString());
+                        team2_name.setText(input.getText().toString());
+                    }
+            );
+        });
 
         //team1_20 = findViewById(R.id.team1_20);
         //team1_50 = findViewById(R.id.team1_50);
@@ -60,6 +82,19 @@ public class GameActivity extends AppCompatActivity {
         //team2_20 = findViewById(R.id.team2_20);
         //team2_50 = findViewById(R.id.team2_50);
         //team2_100 = findViewById(R.id.team2_100);
+    }
+
+    private void showPopUp(String title, String message, EditText input, DialogInterface.OnClickListener listener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(title)
+                .setMessage(message)
+                .setView(input)
+                .setPositiveButton("Ok", listener)
+                .setNegativeButton("Annulé", (d, w) -> {
+                })
+                .create()
+                .show();
     }
 
     /**
